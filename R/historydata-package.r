39156a966f35319e7572a6f98e0008477138bad5
Lincoln Mullen <lincoln@lincolnmullen.com>
2014-09-21 22:07:57 -0400

    Package documentation

--- R/historydata-package.r
+++ R/historydata-package.r
@@ -1,4 +1,11 @@
-#' historydata.
+#' Datasets for historians
+#'
+#' This package provides sample datasets of interest to historians, analogous to
+#' the \code{datasets} package for R generally and the \code{histdata} package
+#' for datasets from the history of statistics.
+#'
+#' @section References: Citations to the sources for datasets are provided in
+#'   the documentation for each dataset.
 #'
 #' @name historydata
 #' @docType package
