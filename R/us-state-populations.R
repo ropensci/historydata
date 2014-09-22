5854dd60b22224b0fc6649e0ac4fcaa3cd883b92
Lincoln Mullen <lincoln@lincolnmullen.com>
2014-09-21 23:28:51 -0400

    Populations for US states and territories

---
+++ R/us-state-populations.R
@@ -0,0 +1,26 @@
+#' Populations of US states and territories, 1790-2010
+#'
+#' Population figures for US states and territories from the decennial census.
+#'
+#' @section Variables:
+#'
+#'   \itemize{
+#'
+#'   \item \code{year}: date of the census.
+#'
+#'   \item \code{state}: name of the state or territory.
+#'
+#'   \item \code{population}: population of the state or territory.
+#'
+#'   \item \code{GISJOIN}: a unique identifier for joining NHGIS data to spatial
+#'   data.
+#'
+#'   }
+#'
+#' @section Citation: This dataset has been gathered by the
+#'   \href{https://www.nhgis.org/}{NHGIS}. Minnesota Population Center,
+#'   \emph{National Historical Geographic Information System: Version 2.0}
+#'   (Minneapolis: University of Minnesota, 2011).
+#' @examples
+#' head(us_state_populations)
+"us_state_populations"
