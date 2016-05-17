cd0fc3846e079529440051c9971803ecf1f69165
Lincoln Mullen <lincoln@lincolnmullen.com>
2016-05-17 19:24:03 -0400

    Add CESTA cities dataset

---
+++ R/us_cities_pop.R
@@ -0,0 +1,13 @@
+#' United States Historical City Populations, 1790-2010
+#'
+#' A dataset of cities mentioned in the U.S. Census and other data sources. In
+#' general, cities are included once they reach a population of 2,500. Data is
+#' recorded every ten years. See
+#' \href{https://github.com/cestastanford/historical-us-city-populations/}{the
+#' dataset's GitHub page} for details on how the data was gathered.
+#'
+#' @references Erik Steiner and Jason Heppler, "United States Historical City
+#'   Populations, 1790-2010," Spatial History Project, Center for Spatial and
+#'   Textual Analysis, Stanford University,
+#'   \url{https://github.com/cestastanford/historical-us-city-populations/}
+"us_cities_pop"
---
+++ data-raw/cesta-cities.R
@@ -0,0 +1,40 @@
+library(readr)
+library(dplyr)
+library(tidyr)
+
+if (!file.exists("data-raw/cesta-cities.csv")) {
+  download.file("https://raw.githubusercontent.com/cestastanford/historical-us-city-populations/master/data/1790-2010_MASTER.csv",
+                destfile = "data-raw/cesta-cities.csv")
+}
+
+cities <- read_csv("data-raw/cesta-cities.csv")
+
+cities <- cities %>%
+  gather(year, population, -ID, -ST, -City, -CityST, -STPLFIPS_2010, -Name_2010,
+         -County, -LAT, -LON, -LAT_BING, -LON_BING, -`City Source`,
+         -`Population Source`, -`Place Type`, -`County_Name`)
+
+names(cities) <- tolower(names(cities))
+
+cities <- cities %>%
+  rename(source_city = `city source`, source_population = `population source`,
+         place_type = `place type`) %>%
+  select(id, city, state = st, year, population, everything())
+
+correct_na <- function(x) {
+  if (is.character(x)) {
+    out <- ifelse(x == "0", NA, x)
+  } else if (is.numeric(x)) {
+    out <- ifelse(x == 0, NA, x)
+  }
+  out
+}
+
+cities <- cities %>%
+  filter(population > 0) %>%
+  mutate(stplfips_2010 = correct_na(stplfips_2010),
+         name_2010 = correct_na(name_2010))
+
+us_cities_pop <- cities
+
+devtools::use_data(us_cities_pop, overwrite = TRUE)
