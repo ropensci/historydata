fc7f5b09dbb9e5a23c8f3bd17203a3e4daeced95
lm-hien <minhhien2872002@gmail.com>
2025-07-04 08:31:47 +0700

    Update us_state_populations to include the year 2020

--- data-raw/scripts/us-state-populations.R
+++ data-raw/scripts/us-state-populations.R
@@ -1,10 +1,20 @@
 library(dplyr)
-us_state_populations <- read.csv(file.path("data-raw", "nhgis0011_ts_state.csv"),
+library(tidyr)
+us_state_populations <- read.csv(file.path("data-raw", "nhgis0001_ts_nominal_state.csv"),
                                  stringsAsFactors = FALSE)
 us_state_populations <- us_state_populations %>%
+  pivot_longer(
+    cols = matches("A00AA\\d{4}$"),
+    names_to = "year",
+    values_to = "population",
+    names_pattern = "A00AA(\\d{4})"
+  ) %>%
+  drop_na(population) %>%
+  mutate(year = as.integer(year)) %>%
   select(GISJOIN,
-         year = YEAR,
+         year,
          state = STATE,
-         population = A00AA) %>%
+         population) %>%
+  arrange(year, state) %>%
   as_tibble()
 usethis::use_data(us_state_populations, overwrite = TRUE)
